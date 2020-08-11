83cac10c87bc4b3591764894b7903066ee9a1dd5
Volkan Arıslı <volkanarisli@gmail.com>
2020-08-11 11:03:49 +0300

    Initial commit

---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+const StramCreate = () =>
+{
+  return <div>Stream Create</div>
+}
+
+export default StramCreate;
---
+++ src/components/streams/StreamShow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+const StramShow = () =>
+{
+  return <div>Stream Show</div>
+}
+
+export default StramShow;
